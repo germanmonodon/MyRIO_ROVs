855055a081abd21de95656d4f26308143f08b111
unknown <german@monodon.es>
2023-03-28 15:09:32 +0200

    First commit

---
+++ Spectogram.py
@@ -0,0 +1,26 @@
+import os
+import wave
+
+import pylab
+
+def graph_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram.png')
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.frombuffer(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+
+
+
+
+if __name__ == '__main__':
+    graph_spectrogram("Data/Recording_2023_03_28_10_23_02.wav")
+
